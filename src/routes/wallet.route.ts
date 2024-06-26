3e3087435e2975a270d434a60c3a92ed411a2262
PolyMath-Coder <you@example.com>
2024-06-26 14:17:24 +0100

    chore: input validation

--- src/routes/wallet.route.ts
+++ src/routes/wallet.route.ts
@@ -1,12 +1,52 @@
 import { Router } from "express";
 import walletController from "../controllers/wallet.controller";
 import userAuthentication from "../helpers/auth/auth";
+import { check } from "express-validator";
+import inputValidator from "../helpers/validator/validate";
 
 const walletRouter = Router()
 
-walletRouter.post('/fund-wallet', userAuthentication, walletController.fundWallet)
+// [
+//     check('temp_token')
+//         .isLength({ min: 10, max: 40 })
+//         .withMessage('Email token required')
+//         .trim(),
+walletRouter.post('/fund-wallet',
+    [
+        check('amount')
+        .isNumeric()
+        .withMessage('valid amount to fund wallet required')
+        .trim()
+    ],
+    inputValidator,
+    userAuthentication, 
+    walletController.fundWallet)
 
-walletRouter.post('/fund/transfer', userAuthentication, walletController.fundTransfer)
+walletRouter.post('/fund/transfer', 
+    [
+        check('amount')
+        .isNumeric()
+        .withMessage('valid amount to transfer required')
+        .trim(),
 
-walletRouter.post('/withdraw-fund', userAuthentication, walletController.fundWithdrawal)
+        check('userId')
+        .isNumeric()
+        .withMessage('valid id of beneficiary required')
+        .trim()  
+    ],
+    inputValidator,
+    userAuthentication, 
+    walletController.fundTransfer)
+
+walletRouter.post('/withdraw-fund', 
+    [
+        check('amount')
+        .isNumeric()
+        .withMessage('valid amount to transfer required')
+        .trim(),
+
+    ],
+    inputValidator,
+    userAuthentication, 
+    walletController.fundWithdrawal)
 export default walletRouter;
