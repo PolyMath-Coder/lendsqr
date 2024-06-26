a42486f444977ddafb42a053ffe7012e68b2d2a5
PolyMath-Coder <you@example.com>
2024-06-26 13:34:52 +0100

    chore: include db credentials

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -4,7 +4,11 @@ config()
 const configInv = {
     JWT_SECRET: process.env.JWT_SECRET,
     LENDSQR_SECRET_KEY: process.env.LENDSQR_SECRET_KEY,
-    ADJUTOR_BASE_URL: process.env.ADJUTOR_BASE_URL
+    ADJUTOR_BASE_URL: process.env.ADJUTOR_BASE_URL,
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DATABASE: process.env.DATABASE
 
 }
 
